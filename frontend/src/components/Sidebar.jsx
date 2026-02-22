4ca3a2d3403305a43c5ca20206d5f9c698411388
Yash Jagtap <149660118+Yaxhjagtap@users.noreply.github.com>
2026-02-22 23:11:56 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,16 +1,45 @@
 import { NavLink } from "react-router-dom";
 import "../styles/dashboard.css";
+
 export default function Sidebar() {
   return (
     <aside className="sidebar">
       <h2>CodePathshala</h2>
 
-      <NavLink to="/dashboard">Home</NavLink>
-      <NavLink to="/quests">Quest Map</NavLink>
-      <NavLink to="/code">Code Lab</NavLink>
-      <NavLink to="/leaderboard">Leaderboard</NavLink>
-      <NavLink to="/mentor">AI Mentor</NavLink>
-      <NavLink to="/avatar">My Avatar</NavLink>
+      <NavLink to="/dashboard">
+        <span>🏠</span> Home
+      </NavLink>
+      <NavLink to="/quests">
+        <span>🗺️</span> Quest Map
+      </NavLink>
+       <NavLink 
+          to="/labs" 
+          className={({ isActive }) => 
+            `nav-item ${isActive ? "active" : ""}`
+          }
+        >
+          <span className="nav-icon">🧪</span>
+          <span className="nav-text">Coding Labs</span>
+        </NavLink>
+        
+        <NavLink 
+          to="/code" 
+          className={({ isActive }) => 
+            `nav-item ${isActive ? "active" : ""}`
+          }
+        >
+          <span className="nav-icon">💻</span>
+          <span className="nav-text">Code editor</span>
+        </NavLink>
+      <NavLink to="/leaderboard">
+        <span>🏆</span> Leaderboard
+      </NavLink>
+      <NavLink to="/mentor">
+        <span>🤖</span> AI Mentor
+      </NavLink>
+      <NavLink to="/avatar">
+        <span>👤</span> My Avatar
+      </NavLink>
     </aside>
   );
 }
