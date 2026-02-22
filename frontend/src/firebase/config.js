d6bb5f4fad49e970b5757f5bcbc2be7b1a93bbb2
Yash Jagtap <149660118+Yaxhjagtap@users.noreply.github.com>
2026-02-22 23:19:12 +0530

    Update config.js

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -2,8 +2,15 @@ import { initializeApp } from "firebase/app";
 // Analytics is OPTIONAL – safe to remove in hackathons
 // import { getAnalytics } from "firebase/analytics";
 
-
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAfr4qgbXGpLTnkj03qztq4N9F-Ha74d_A",
+  authDomain: "codepathshala-6e08d.firebaseapp.com",
+  projectId: "codepathshala-6e08d",
+  storageBucket: "codepathshala-6e08d.firebasestorage.app",
+  messagingSenderId: "321930509374",
+  appId: "1:321930509374:web:f4e28b9690a717ab2aa56f",
+  measurementId: "G-7NMXYZVRWL"
+};
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
