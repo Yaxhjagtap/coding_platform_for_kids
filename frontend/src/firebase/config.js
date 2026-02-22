be91845cdcaf7d67e2c1368b43ebdfde4395bfc1
Yash Jagtap <149660118+Yaxhjagtap@users.noreply.github.com>
2026-02-22 23:05:56 +0530

    Remove sensitive Firebase config from config.js
    Removed Firebase configuration details for security.

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 // Analytics is OPTIONAL – safe to remove in hackathons
 // import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCgqUTGB-aF3G12Ck-ibYfKs4qhV_5Lm3M",
-  authDomain: "codepathshala-6f9f4.firebaseapp.com",
-  projectId: "codepathshala-6f9f4",
-  storageBucket: "codepathshala-6f9f4.firebasestorage.app",
-  messagingSenderId: "787528605413",
-  appId: "1:787528605413:web:32eed41cf5b7cb56223e0c",
-  measurementId: "G-BJFWN5CHMQ"
-};
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
